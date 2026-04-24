5c5abd99bbd111964baebc6bd9d2d10978cc46ab
AnaSofiR <asrodriguo@eafit.edu.co>
2026-04-24 12:35:53 -0500

    v0.1 Solve e2e tests problems

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,5 +13,7 @@ export default defineConfig({
     command: "npm run preview",
     url: "http://localhost:4173",
     reuseExistingServer: !process.env.CI,
+    stdout: 'ignore',
+    stderr: 'pipe',
   },
 });
